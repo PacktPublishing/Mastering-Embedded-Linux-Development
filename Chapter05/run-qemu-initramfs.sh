af6f05838b53450febd9deb2c0830f72dd9465ea
Frank Vasquez <Frank.Vasquez@Gmail.com>
2024-08-31 21:38:34 -0700

    Update run-qemu-initramfs.sh

--- Chapter05/run-qemu-initramfs.sh
+++ Chapter05/run-qemu-initramfs.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-# Copy this script to and run it from your linux-stable directory
 
-KERNEL=arch/arm64/boot/Image
+KERNEL=${HOME}/linux-stable/arch/arm64/boot/Image
 INITRAMFS=${HOME}/initramfs.cpio.gz
 
 if [ ! -f ${KERNEL} ]; then
-	echo "${KERNEL} does not exist"
-	exit 1
+    echo "${KERNEL} does not exist"
+    exit 1
 fi
 
 if [ ! -f ${INITRAMFS} ]; then
-	echo "${INITRAMFS} does not exist"
-	exit 1
+    echo "${INITRAMFS} does not exist"
+    exit 1
 fi
 
-QEMU_AUDIO_DRV=none \
 qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 -kernel ${KERNEL} -append "console=ttyAMA0 rdinit=/bin/sh" -initrd ${INITRAMFS}
