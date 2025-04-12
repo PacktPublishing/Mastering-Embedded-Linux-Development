50e205e88fb2abb08f99a10934ec9958850aa1c4
Frank Vasquez <frank.vasquez@gmail.com>
2025-04-12 15:41:36 -0700

    Repair exercise for creating a custom BSP with Buildroot

--- Chapter06/buildroot/board/meld/nova/post-image.sh
+++ Chapter06/buildroot/board/meld/nova/post-image.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
 
-cp ${BOARD_DIR}/uEnv.txt $BINARIES_DIR/uEnv.txt
 cp ${BUILD_DIR}/ti-k3-r5-loader-2022.10/tiboot3.bin $BINARIES_DIR/tiboot3.bin
 
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg" GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
